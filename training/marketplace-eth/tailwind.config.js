9643140a18821a8ad85b5d02bcf8d491942f81e1
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-08 10:39:17 -0700

    Added marketplace-eth training project

---
+++ training/marketplace-eth/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // Example content paths...
+    './pages/**/*.{js,ts,jsx,rsx}',
+    './components/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
