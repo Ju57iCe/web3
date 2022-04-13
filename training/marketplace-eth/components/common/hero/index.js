247b4269d7bcb921b0550822a820296e29d8c7e0
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-13 01:49:01 -0700

    Separated HTML into components Added common base layout for all pages

--- training/marketplace-eth/components/common/hero/index.js
+++ training/marketplace-eth/components/common/hero/index.js
@@ -1,6 +1,7 @@
 export default function Hero() {
-    return (
-      <section className="lg:2/6 text-left my-28">
+
+  return (
+    <section className="lg:2/6 text-left my-28">
       <div className="text-6xl font-semibold text-gray-900 leading-none">Grow your career as a developer</div>
       <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Learn programming and web development the easy way! Get unlimited access to all of our courses.</div>
       <div className="mt-5 sm:mt-8 flex lg:justify-start">
@@ -11,5 +12,5 @@ export default function Hero() {
         </div>
       </div>
     </section>
-    )
+  )
 }
---
+++ training/marketplace-eth/pages/course.js
@@ -0,0 +1,20 @@
+import { Modal } from "@components/common";
+import {
+  CourseHero,
+  Keypoints,
+  Curriculum
+} from "@components/course";
+import { BaseLayout } from "@components/layout";
+
+export default function Course() {
+    return (
+      <BaseLayout>
+        <div className="py-4">
+          <CourseHero />
+        </div>
+        <Keypoints />
+        <Curriculum />
+        <Modal />
+      </BaseLayout>
+    )
+  }
