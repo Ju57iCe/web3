ff5535f8dda48b08201d011a3984d0593276cabe
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-15 05:54:45 -0700

    Added address greetings Added hiding of address in marketplace page

--- training/marketplace-eth/components/ui/web3/hooks/useAccount.js
+++ training/marketplace-eth/components/ui/web3/hooks/useAccount.js
@@ -1,22 +1,34 @@
 import { useState, useEffect } from "react"
+import useSWR from "swr"
+
+const adminAdresses = {
+  "0x339bdac0ae9e5e35aace9f5c6f7b43b124a29ab7366e6eb669a8d2c840983646": true
+}
 
 export const handler = (web3, provider) => () => {
-  const [account, setAccount] = useState(null)
 
-  useEffect(() => {
-    const getAccount = async () => {
+  const { data, mutate, ...rest } = useSWR(() =>
+    web3 != null ? "web3/accounts" : null,
+    async () => {
       const accounts = await web3.eth.getAccounts()
-      setAccount(accounts[0])
+      return accounts[0]
     }
-
-    web3 && getAccount()
-  }, [web3])
+  )
 
   useEffect(() => {
     provider &&
     provider.on("accountsChanged",
-      accounts => setAccount(accounts[0] ?? null))
+      accounts => mutate(accounts[0] ?? null))
   }, [provider])
 
-  return { account }
+  return {
+    account: {
+      data,
+      isAdmin: (
+        data &&
+        adminAdresses[web3.utils.keccak256(data)]) ?? false,
+      mutate,
+      ...rest
+    }
+  }
 }
--- training/marketplace-eth/pages/marketplace/index.js
+++ training/marketplace-eth/pages/marketplace/index.js
@@ -1,12 +1,18 @@
-import { Hero } from "@components/ui/common"
 import { CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
+import { WalletBar } from "@components/ui/web3"
+import { useAccount } from "@components/hooks/web3/useAccount"
 
-export default function Home({courses}) {
+export default function Marketplace({courses}) {
+  const { account } = useAccount()
+
   return (
     <>
-      <Hero />
+      <div className="py-4">
+        <WalletBar
+          address={account.data}/>
+      </div>
       <CourseList
         courses={courses} />
     </>
@@ -22,4 +28,4 @@ export function getStaticProps() {
   }
 }
 
-Home.Layout = BaseLayout
+Marketplace.Layout = BaseLayout
