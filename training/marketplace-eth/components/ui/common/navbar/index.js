19f8f08db5aebbf9294aee9b1f0dbf8e9d88ce60
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-15 05:12:52 -0700

    Added proper displaying of the connected metamask address

--- training/marketplace-eth/components/ui/common/navbar/index.js
+++ training/marketplace-eth/components/ui/common/navbar/index.js
@@ -10,57 +10,69 @@ export default function Navbar() {
 
   return (
       <section>
-        {account}
-      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-        <nav className="relative" aria-label="Global">
-          <div className="flex justify-between items-center">
-            <div>
-              <Link href="/">
-              <a
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                Home
-              </a>
-              </Link>
-              <Link href="/">
-              <a
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                Marketplace
-              </a>
-              </Link>
-              <Link href="/">
-              <a
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                Blog
-              </a>
-              </Link>
-            </div>
-            <div>
-            <Link href="/">
-              <a
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                Wishlist
-              </a>
-            </Link>
-            { isLoading ?
-              <Button
-                disabled={true}
-                onClick={connect}>
-                Loading...
-              </Button> : 
-                isWeb3Loaded ?
+        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <nav className="relative" aria-label="Global">
+            <div className="flex justify-between items-center">
+              <div>
+                <Link href="/">
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+                </Link>
+                <Link href="/">
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Marketplace
+                </a>
+                </Link>
+                <Link href="/">
+                <a
+                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blog
+                </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <a
+                    className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                    Wishlist
+                  </a>
+                </Link>
+                { isLoading ?
                   <Button
+                    disabled={true}
                     onClick={connect}>
-                    Connect
-                  </Button> :
-                  <Button
-                    onClick={() => window.open("https://metamask.io/download/", "_blank")}>
-                    Install Metamask
-                  </Button>
-            }
-            </div>
+                    Loading...
+                  </Button> : 
+                    isWeb3Loaded ?
+                    account ? 
+                    <Button
+                      hoverable={false}
+                      className="cursor-default">
+                      Hi there
+                    </Button> :
+                    <Button
+                      onClick={connect}>
+                      Connect
+                    </Button> :
+                    <Button
+                      onClick={() => window.open("https://metamask.io/download/", "_blank")}>
+                      Install Metamask
+                    </Button>
+                }
+              </div>
           </div>
         </nav>
       </div>
+      { account &&
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            { account }
+          </div>
+        </div>
+      }
     </section>
   )
 }
