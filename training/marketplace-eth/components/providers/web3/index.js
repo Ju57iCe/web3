cd4199a27578c762c8c9d98a4bd98f9cfbf81ce6
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-20 10:54:53 -0700

    Added loading of contracts Refactored hooks

--- training/marketplace-eth/components/providers/web3/index.js
+++ training/marketplace-eth/components/providers/web3/index.js
@@ -1,19 +1,31 @@
 const { createContext, useContext, useEffect, useState, useMemo } = require("react");
 
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "@utils/loadContract";
 import Web3 from "web3";
 import { setupHooks } from "./hooks/setupHooks";
 
 const Web3Context = createContext(null)
 
+const createWeb3State = ({web3, provider, contract, isLoading}) => {
+  return {
+    web3,
+    provider,
+    contract,
+    isLoading,
+    hooks: setupHooks({web3, provider, contract})
+  }
+}
+
 export default function Web3Provider({children}) {
-  const [web3Api, setWeb3Api] = useState({
-    provider: null,
-    web3: null,
-    contract: null,
-    isLoading: true,
-    hooks: setupHooks()
-  })
+  const [web3Api, setWeb3Api] = useState(
+    createWeb3State({
+      web3: null,
+      provider: null,
+      contract: null,
+      isLoading: true
+    })
+  )
 
   useEffect(() => {
     const loadProvider = async () => {
@@ -21,15 +33,23 @@ export default function Web3Provider({children}) {
       const provider = await detectEthereumProvider()
       if (provider) {
         const web3 = new Web3(provider)
+        const contract = await loadContract("CourseMarketplace", web3)
         setWeb3Api({
           provider,
           web3,
-          contract: null,
+          contract,
           isLoading: false,
-          hooks: setupHooks(web3, provider)
+          hooks: setupHooks({web3, provider, contract})
         })
       } else {
-        setWeb3Api(api => ({...api, isLoading: false}))
+        setWeb3Api(
+          createWeb3State({
+            web3,
+            provider,
+            contract,
+            isLoading: False
+          })
+        )
         console.error("Please, install Metamask.")
       }
     }
@@ -39,7 +59,6 @@ export default function Web3Provider({children}) {
 
   const _web3Api = useMemo(() => {
     const { web3, provider, isLoading } = web3Api
-
     return {
       ...web3Api,
       requireInstall: !isLoading && !web3 ,
--- training/marketplace-eth/pages/marketplace/index.js
+++ training/marketplace-eth/pages/marketplace/index.js
@@ -1,17 +1,42 @@
-
-
 import { CourseCard, CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { useWalletInfo } from "@components/hooks/web3"
+import { Button } from "@components/ui/common"
 import { OrderModal } from "@components/ui/order"
 import { useState } from "react"
 import { MarketHeader } from "@components/ui/marketplace"
-import { Button } from "@components/ui/common"
+import { useWeb3 } from "@components/providers"
 
 export default function Marketplace({courses}) {
+  const { web3, contract } = useWeb3()
+  const { canPurchaseCourse, account } = useWalletInfo()
   const [selectedCourse, setSelectedCourse] = useState(null)
-  const { canPurchaseCourse } = useWalletInfo()
+
+  const purchaseCourse = async order => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
+    const orderHash = web3.utils.soliditySha3(
+      { type: "bytes16", value: hexCourseId },
+      { type: "address", value: account.data }
+    )
+    const emailHash = web3.utils.sha3(order.email)
+    const proof = web3.utils.soliditySha3(
+      { type: "bytes32", value: emailHash },
+      { type: "bytes32", value: orderHash }
+    )
+
+    const value = web3.utils.toWei(String(order.price))
+
+    try {
+      const result = await contract.methods.purchaseCourse(
+        hexCourseId,
+        proof
+      ).send({from: account.data, value})
+      console.log(result)
+    } catch {
+      console.error("Purchase course: Operation has failed.")
+    }
+  }
 
   return (
     <>
@@ -23,10 +48,10 @@ export default function Marketplace({courses}) {
       >
       {course =>
         <CourseCard
-        key={course.id}
-        course={course}
-        disabled={!canPurchaseCourse}
-        Footer={() =>
+          key={course.id}
+          course={course}
+          disabled={!canPurchaseCourse}
+          Footer={() =>
             <div className="mt-4">
               <Button
                 onClick={() => setSelectedCourse(course)}
@@ -42,6 +67,7 @@ export default function Marketplace({courses}) {
       { selectedCourse &&
         <OrderModal
           course={selectedCourse}
+          onSubmit={purchaseCourse}
           onClose={() => setSelectedCourse(null)}
         />
       }
