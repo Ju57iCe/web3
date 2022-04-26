e5a92c137629a0ab50742bc2faf5633ece0a909c
Ju57iCe <st.z.atanasov@gmail.com>
2022-04-26 07:34:37 -0700

    Added hooks for retrieving owned courses from the smart contract

--- training/marketplace-eth/components/hooks/web3/index.js
+++ training/marketplace-eth/components/hooks/web3/index.js
@@ -36,6 +36,14 @@ export const useAccount = () => {
   }
 }
 
+export const useManagedCourses = (...args) => {
+  const swrRes = enhanceHook(useHooks(hooks => hooks.useManagedCourses)(...args))
+
+  return {
+    managedCourses: swrRes
+  }
+}
+
 export const useOwnedCourses = (...args) => {
   const swrRes = enhanceHook(useHooks(hooks => hooks.useOwnedCourses)(...args))
 
